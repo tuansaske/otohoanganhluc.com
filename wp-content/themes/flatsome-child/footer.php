4dd97a6b8fce75f6018f65c4c64dc773cb69e262
Nguyễn Anh Tuấn <nguyenanhtuanproduction@gmail.com>
2019-04-18 16:17:24 +0700

    Revert "ver 1.0.2 17042019"
    This reverts commit 0a5ebb0588717a39c03909031ad3d915b3df93c5.

--- wp-content/themes/flatsome-child/footer.php
+++ wp-content/themes/flatsome-child/footer.php
@@ -40,12 +40,12 @@ global $flatsome_opt;
     .hotline{ 
         display :none;
     }
-    .mobile-hotline{display: block; bottom: 0; width: 100%; background:rgba(0,0,0,0.5); height: 40px; position: fixed; z-index:9999999}
-    .mobile-hotline .mobile-hotline-left{width: 45%; float: left; text-align: center; background: #0075ff; margin-left: 10px; margin-right:5px; margin-top: 7px; height: 25px; border-radius: 4px}
-    .mobile-hotline .mobile-hotline-left a{color: white;line-height: 16px; font-size:12px; font-weight: bold}
+    .mobile-hotline{display: block; bottom: 0; width: 100%; background:rgba(0,0,0,0.5); height: 60px; position: fixed; z-index:9999999}
+    .mobile-hotline .mobile-hotline-left{width: 45%; float: left; text-align: center; background: #0075ff; margin-left: 10px; margin-right:5px; margin-top: 7px; height: 45px; border-radius: 4px}
+    .mobile-hotline .mobile-hotline-left a{color: white; line-height: 46px; font-size:16px; font-weight: bold}
      
-    .mobile-hotline .mobile-hotline-right{width: 45%; float: right; text-align: center; background: #ed1b23; margin-left: 5px; margin-right: 10px; margin-top: 7px; height: 25px; border-radius: 4px}
-    .mobile-hotline .mobile-hotline-right a{color: #fff; font-size: 12px; line-height: 16px; font-weight: bold}
+    .mobile-hotline .mobile-hotline-right{width: 45%; float: right; text-align: center; background: #fac100; margin-left: 5px; margin-right: 10px; margin-top: 7px; height: 45px; border-radius: 4px}
+    .mobile-hotline .mobile-hotline-right a{color: red; line-height: 46px; font-size: 16px; font-weight: bold}
 }
 </style>
 <a href="tel:+84905122767">
